70fd2d36c074eee89cd9ba9ea7c7c4f2f77c70ba
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-02-01 10:18:07 +0100

    database

--- inlogregistratiesysteem/connect_db.php
+++ inlogregistratiesysteem/connect_db.php
@@ -3,9 +3,9 @@
   // We hebben hieronder constanten gedefinieerd voor de
   // servernaam, user, password en databasenaam.
   define("SERVER_NAME", "localhost");
-  define("USER_NAME", "rra_loginregistration");
+  define("USER_NAME", "rra_loginreg18");
   define("PASSWORD", "Geheim!");
-  define("DATABASE_NAME", "rra_loginreg18");
+  define("DATABASE_NAME", "am1b_loginregistration");
   // Met myqli_connect maken we contact met onze mysql-server.
   $conn = mysqli_connect(SERVER_NAME, USER_NAME, PASSWORD, DATABASE_NAME);
   // ***********************************************************************
--- inlogregistratiesysteem/register.php
+++ inlogregistratiesysteem/register.php
@@ -1,4 +1,28 @@
 <?php
   echo "Registratie";
   var_dump($_POST);
+
+  include("./connect_db.php");
+  include("./functions/functions.php");
+
+  $email = sanitize($_POST["email"]);
+  $conditions = sanitize($_POST["conditions"]);
+
+  $sql = "INSERT INTO `login` (`id`,
+                               `email`,
+                               `conditions`) 
+                       VALUES (NULL,
+                               '{$email}',
+                               '{$conditions}')";
+
+  // Vuur de query af op de database
+  mysqli_query($conn, $sql);
+
+  // Hiermee wordt de verbinding verbroken.
+  mysqli_close($conn);
+  
+  // Met deze code kun je even blijven op de pagina en dan teruggaan
+  // header("refresh:40; url=./index.php");
+  // Je wordt nu direct teruggestuurd naar index.php
+  header("Location: ./index.html");
 ?>
