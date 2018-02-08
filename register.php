64a5ecbe963713a750153caaf96ea137a2adf84b
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-02-08 09:56:42 +0100

    empty email

--- register.php
+++ register.php
@@ -9,37 +9,41 @@
     $email = sanitize($_POST["email"]);
     $conditions = sanitize($_POST["conditions"]);
 
-    // Maak een selectie query om te kijken in de database of het emailadres al bestaat
-    $sql = "SELECT * FROM `login` WHERE `email` = '{$email}'";
-
-    // Vuur de query af op de database
-    $result = mysqli_query($conn, $sql);
-    
-    // Tel het aantal rijen dat voldoet aan de selectie query
-    $number_of_rows = mysqli_num_rows($result);
-
-    // Als mysqli_num_rows aantal gevonden rijen 1 teruggeeft dan...
-    if (mysqli_num_rows($result)) {
-      // Echo dat het emailadres al bestaat anders...
-      header("Location: ./index.php?status=emailexists");
+    if (empty($_POST["email"])) {
+      header("Location: ./index.php?status=empty_email");
     } else {
-      $sql = "INSERT INTO `login` (`id`,
-                                  `email`,
-                                  `conditions`,
-                                  `userrole`) 
-                              VALUES (NULL,
-                                  '{$email}',
-                                  '{$conditions}',
-                                  'subscriber')";
+      // Maak een selectie query om te kijken in de database of het emailadres al bestaat
+      $sql = "SELECT * FROM `login` WHERE `email` = '{$email}'";
 
       // Vuur de query af op de database
-      mysqli_query($conn, $sql);
+      $result = mysqli_query($conn, $sql);
+      
+      // Tel het aantal rijen dat voldoet aan de selectie query
+      $number_of_rows = mysqli_num_rows($result);
 
-      // Hiermee wordt de verbinding verbroken.
-      mysqli_close($conn);
+      // Als mysqli_num_rows aantal gevonden rijen 1 teruggeeft dan...
+      if (mysqli_num_rows($result)) {
+        // Echo dat het emailadres al bestaat anders...
+        header("Location: ./index.php?status=emailexists");
+      } else {
+        $sql = "INSERT INTO `login` (`id`,
+                                    `email`,
+                                    `conditions`,
+                                    `userrole`) 
+                                VALUES (NULL,
+                                    '{$email}',
+                                    '{$conditions}',
+                                    'subscriber')";
 
-      // Je wordt nu direct teruggestuurd naar index.php
-      header("Location: ./index.php?status=succes");
+        // Vuur de query af op de database
+        mysqli_query($conn, $sql);
+
+        // Hiermee wordt de verbinding verbroken.
+        mysqli_close($conn);
+
+        // Je wordt nu direct teruggestuurd naar index.php
+        header("Location: ./index.php?status=succes");
+      }
     }
   } else {
     header("Location: ./index.php?status=conditions_not_checked");
