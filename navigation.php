dbee676ab1ff608aaff18ce0297b53a77efde514
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-02-15 09:38:11 +0100

    include navigation

---
+++ navigation.php
@@ -0,0 +1,5 @@
+<main class="container">
+  <a href="http://www.inlogregistratiesysteem.am1b.nl/index.php?action=home">Home</a>
+  <a href="http://www.inlogregistratiesysteem.am1b.nl/index.php?action=registerform">Registreer</a>
+  <a href="http://www.inlogregistratiesysteem.am1b.nl/index.php?action=games">Games</a>
+</main>
