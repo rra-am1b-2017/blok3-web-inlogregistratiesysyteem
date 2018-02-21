3c74d492c93cdc57fd6164f929c317b5b85f95d5
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2018-02-21 14:18:12 +0100

    password

--- choosepassword.php
+++ choosepassword.php
@@ -1,38 +1,33 @@
 <main class="container">
-  <h3>Activeer uw account</h3>
+
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-          </div>
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+      <form action="./activate.php" method="post">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Activeren &amp; wachtwoord</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <div class="form-group">
+              <label for="exampleInputPassword1">Wachtwoord</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Wachtwoord" name="password">
+              <small id="passwordHelp" class="form-text text-muted">Kies een geschikt wachtwoord.</small>            
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword2">Bevestig wachtwoord</label>
+              <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Bevestig wachtwoord" name="confirmPassword">
+              <small id="passwordHelp" class="form-text text-muted">Herhaal uw gekozen wachtwoord.</small>            
+            </div>
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-primary">Verzenden</button>        
+        </div>
+      </form>
     </div>
   </div>
 </div>
